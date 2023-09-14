52b810ce2d39b412529c194be33a1fcedaf15839
ReplayCoding <replaycoding@gmail.com>
2023-09-13 20:20:24 -0700

    beginnings of an eh_frame parser

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use std::borrow::Cow;
+use std::io;
+use std::io::Cursor;
+use std::io::ErrorKind;
 use std::io::Read;
+use std::io::Seek;
 use std::path::Path;
 use std::env;
 use std::fs;
 
-use gimli::BaseAddresses;
-use gimli::UnwindSection;
-use gimli::{self, Section};
+use byteorder::ByteOrder;
+use byteorder::LittleEndian;
+use byteorder::ReadBytesExt;
 use object::{Object, ObjectSection};
 
 fn hexdump(data: &[u8], chunk_size: usize) {
@@ -15,6 +18,7 @@ fn hexdump(data: &[u8], chunk_size: usize) {
             print!("{:02x} ", b);
         }
 
+        // padding to align the ascii display
         for _ in 0..chunk_size - chunk.len() {
             print!("   ");
         }
@@ -28,43 +32,51 @@ fn hexdump(data: &[u8], chunk_size: usize) {
                 print!(".");
             }
         }
+
         print!("\n");
     }
 }
 
-fn dump_eh_frame(object: &object::File, endian: gimli::RunTimeEndian) {
-    // Load a section and return as `Cow<[u8]>`.
-    let load_section = |id: gimli::SectionId| -> Result<Cow<[u8]>, gimli::Error> {
-        match object.section_by_name(id.name()) {
-            Some(ref section) => Ok(section
-                .uncompressed_data()
-                .unwrap_or(Cow::Borrowed(&[][..]))),
-            None => Ok(Cow::Borrowed(&[][..])),
-        }
-    };
-
-    let eh_frame_data = load_section(gimli::SectionId::EhFrame).unwrap();
-    hexdump(&eh_frame_data, 16);
-    let eh_frame = gimli::EhFrame::new(&eh_frame_data, endian);
-
-    let mut bases = gimli::BaseAddresses::default();
-    if let Some(section) = object.section_by_name(".eh_frame_hdr") {
-        bases = bases.set_eh_frame_hdr(section.address());
-    }
-    if let Some(section) = object.section_by_name(".eh_frame") {
-        bases = bases.set_eh_frame(section.address());
-    }
-    if let Some(section) = object.section_by_name(".text") {
-        bases = bases.set_text(section.address());
-    }
-
-    let mut entries = eh_frame.entries(&bases);
+fn dump_eh_frame<Endian: ByteOrder, R: Read + Seek>(data: &mut R) -> io::Result<()> {
     loop {
-        match entries.next().unwrap() {
-            None => break,
-            Some(entry) => println!("{:#?}", entry),
+        // Length
+        // A 4 byte unsigned value indicating the length in bytes of the CIE structure, not
+        // including the Length field itself.
+        let mut length: u64 = match data.read_u32::<Endian>() {
+            Ok(l) => Ok(l.into()),
+            // Some compilers don't put a terminator CIE in the section, and so we get an EOF. In
+            // this case we return 0, which will be handled below
+            Err(e) => if e.kind() == ErrorKind::UnexpectedEof { Ok(0) } else { Err(e) },
+        }?;
+
+        // If Length contains the value 0xffffffff, then the length is contained in the Extended
+        // Length field.
+        if length == 0xffffffff {
+            // Extended Length
+            // A 8 byte unsigned value indicating the length in bytes of the CIE structure, not
+            // including the Length and Extended Length fields.
+            length = data.read_u64::<Endian>()?;
         }
+
+        // If Length contains the value 0, then this CIE shall be considered a terminator and
+        // processing shall end.
+        if length == 0 {
+            break;
+        }
+
+        // Used for keeping track of how many bytes we've read, so we can make sure it matches the
+        // CIE length
+        let start_pos = data.stream_position().unwrap();
+
+        let _ = data.seek(io::SeekFrom::Current(length as i64));
+
+        let n_bytes_read = data.stream_position().unwrap() - start_pos;
+        assert_eq!(n_bytes_read, length, "number of bytes read did not match cie length: {} != {}", n_bytes_read, length);
+
+        println!("cie: {:x} {:08x}", length, data.stream_position().unwrap());
     }
+
+    Ok(())
 }
 
 fn main () {
@@ -73,16 +85,14 @@ fn main () {
             let path = Path::new(arg.as_str());
             let buffer = fs::read(path).unwrap();
             let object = object::File::parse(&*buffer).unwrap();
-            let endian = if object.is_little_endian() {
-                gimli::RunTimeEndian::Little
-            } else {
-                gimli::RunTimeEndian::Big
-            };
 
             for section in object.sections() {
-                println!("section: {}", section.name().unwrap());
+                println!("section: {} {:08x}", section.name().unwrap(), section.address());
             }
-            dump_eh_frame(&object, endian);
+            let eh_frame = object.section_by_name(".eh_frame").unwrap().uncompressed_data().unwrap();
+            // hexdump(&eh_frame, 16);
+
+            dump_eh_frame::<LittleEndian, _>(&mut Cursor::new(eh_frame)).unwrap()
         }
     }
 }
