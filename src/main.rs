f7df18aa687ccd6c2c80a7340258059df4ec93f9
ReplayCoding <replaycoding@gmail.com>
2023-09-18 05:53:14 -0700

    dump code for functions

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,15 @@ mod eh_frame;
 use crate::eh_frame::get_fdes;
 use byteorder::LittleEndian;
 
+use iced_x86::Formatter;
 use object::{Object, ObjectSection};
+
+use std::collections::hash_map::DefaultHasher;
+use std::collections::HashMap;
 use std::env;
 use std::fs;
+use std::hash::Hash;
+use std::hash::Hasher;
 use std::io::Cursor;
 use std::path::Path;
 
@@ -39,6 +45,76 @@ fn demangle_symbol(name: &str) -> Option<String> {
     sym.demangle(&cpp_demangle::DemangleOptions::new()).ok()
 }
 
+struct Function {
+    address: u64,
+
+    content: Vec<u8>,
+    hash: u64,
+}
+
+impl Function {
+    fn new(address: u64, content: Vec<u8>) -> Function {
+        let mut hasher = DefaultHasher::new();
+        content.hash(&mut hasher);
+
+        Function {
+            address,
+            content,
+            hash: hasher.finish(),
+        }
+    }
+}
+
+// TODO: don't duplicate data
+fn get_data_at_address<'a, O: Object<'a, 'a>>(
+    object: &'a O,
+    address: u64,
+    size: u64,
+) -> Option<Vec<u8>> {
+    for section in object.sections() {
+        if section.address() > address || (section.address() + section.size()) <= address {
+            continue;
+        }
+
+        return if let Ok(section_data) = section.uncompressed_data() {
+            let relative_address = address - section.address();
+            let data =
+                &section_data[(relative_address as usize)..(relative_address + size) as usize];
+
+            Some(data.to_vec())
+        } else {
+            None
+        };
+    }
+
+    None
+}
+
+fn dump_code(address: u64, code: &[u8], address_size: usize) {
+    let address = 0; // HACK: tf2 code is relocatable, so this is fine
+
+    let mut decoder = iced_x86::Decoder::with_ip(
+        (address_size * 8) as u32,
+        &code,
+        address,
+        iced_x86::DecoderOptions::NONE,
+    );
+
+    let mut formatter = iced_x86::FastFormatter::new();
+
+    let mut output = String::new();
+    let mut instruction = iced_x86::Instruction::default();
+
+    while decoder.can_decode() {
+        decoder.decode_out(&mut instruction);
+
+        output.clear();
+        formatter.format(&instruction, &mut output);
+
+        println!("\t{:016X} {}", instruction.ip(), output);
+    }
+}
+
 fn main() {
     for (i, arg) in env::args().enumerate() {
         if i == 1 {
@@ -46,13 +122,6 @@ fn main() {
             let buffer = fs::read(path).unwrap();
             let object = object::File::parse(&*buffer).unwrap();
 
-            for section in object.sections() {
-                println!(
-                    "section: {} {:08x}",
-                    section.name().unwrap(),
-                    section.address()
-                );
-            }
             let eh_frame = object.section_by_name(".eh_frame").unwrap();
             let eh_frame_data = eh_frame.uncompressed_data().unwrap();
 
@@ -65,6 +134,7 @@ fn main() {
             )
             .unwrap();
 
+            let mut functions: HashMap<String, Function> = HashMap::new();
             let symbol_map = object.symbol_map();
             for fde in fdes {
                 if let Some(symbol) = symbol_map.get(fde.begin) {
@@ -73,11 +143,31 @@ fn main() {
                         name = demangled_name
                     };
 
-                    println!("function {} (@{:08x}) has length {}", name, fde.begin, fde.length);
+                    functions.insert(
+                        name,
+                        Function::new(
+                            fde.begin,
+                            get_data_at_address(&object, fde.begin, fde.length).unwrap(),
+                        ),
+                    );
+                } else {
+                    println!(
+                        "function {:08x} (length {:08x}) has no symbol",
+                        fde.begin, fde.length
+                    );
                 }
-                else {
-                    println!("function {} (length {}) has no symbol", fde.begin, fde.length);
-                }
+            }
+
+            for (name, func) in functions.iter() {
+                println!(
+                    "function {} (@{:08x}) has length {:08x} [hash {:016x}]",
+                    name,
+                    func.address,
+                    func.content.len(),
+                    func.hash,
+                );
+
+                dump_code(func.address, &func.content, pointer_size);
             }
         }
     }
