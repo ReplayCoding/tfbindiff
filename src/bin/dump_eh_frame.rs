41a1d3337ff2a2770cd8b6b3ba862f94e712f750
ReplayCoding <replaycoding@gmail.com>
2024-01-26 19:28:36 -0800

    implement debug util for dumping eh_frame

---
+++ src/bin/dump_eh_frame.rs
@@ -0,0 +1,40 @@
+use std::fs;
+use std::path::Path;
+use std::io::Cursor;
+use byteorder::LittleEndian;
+use object::{Object, ObjectSection};
+use tfbindiff::eh_frame::get_fdes;
+
+fn load_file(filename: &str) -> memmap2::Mmap {
+    let file = fs::File::open(Path::new(filename)).unwrap();
+    unsafe { memmap2::Mmap::map(&file).unwrap() }
+}
+
+fn main() {
+    let args: Vec<_> = std::env::args().collect();
+
+    if args.len() != 2 {
+        println!("Usage: {} <program>", args[0]);
+        return;
+    }
+
+    let data = load_file(&args[1]);
+    let object = object::File::parse(data.as_ref()).unwrap();
+
+    let pointer_size = if object.is_64() { 8 } else { 4 };
+
+    let eh_frame = object.section_by_name(".eh_frame").unwrap();
+    let eh_frame_data = eh_frame.uncompressed_data().unwrap();
+
+    // FIXME: not that it actually matters, but this shouldn't be hardcoded
+    let fdes = get_fdes::<LittleEndian, _>(
+        &mut Cursor::new(eh_frame_data),
+        pointer_size,
+        eh_frame.address(),
+    )
+    .unwrap();
+
+    for fde in fdes {
+        println!("{:08X} len {:04x}", fde.begin, fde.length);
+    }
+}
