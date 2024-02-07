97345d15d6569e768e46b2cc96e9fd7e2f7b2f4b
ReplayCoding <replaycoding@gmail.com>
2024-02-06 19:03:05 -0800

    update flake

--- flake.nix
+++ flake.nix
@@ -1,34 +1,22 @@
 {
-  inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  };
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
 
-  outputs = { self, flake-utils, naersk, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = (import nixpkgs) {
-          inherit system;
-        };
-
-        naersk' = pkgs.callPackage naersk {};
-
-      in rec {
-        # For `nix build` & `nix run`:
-        defaultPackage = naersk'.buildPackage {
-          src = ./.;
-        };
-
-        # For `nix develop`:
-        devShell = pkgs.mkShell {
+  outputs = { self, nixpkgs }:
+    let forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in {
+      # For `nix develop`:
+      devShell = forAllSystems (system:
+        let pkgs = (import nixpkgs) { inherit system; };
+        in pkgs.mkShell {
           nativeBuildInputs = with pkgs; [ rustc cargo rustfmt clippy ];
-          
+
           shellHook = ''
-              export LD_LIBRARY_PATH=/run/opengl-driver/lib/:${pkgs.lib.makeLibraryPath (with pkgs; [libGL libGLU wayland libxkbcommon])}
+            export LD_LIBRARY_PATH=/run/opengl-driver/lib/:${
+              pkgs.lib.makeLibraryPath
+              (with pkgs; [ libGL libGLU wayland libxkbcommon ])
+            }
           '';
-        };
-      }
-    );
+        });
+    };
 }
 
